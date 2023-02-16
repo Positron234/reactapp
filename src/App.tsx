acec251e843cba7512c799dfa1938a775f24b66a
amitafw <amit.afw1@gmail.com>
2023-02-16 11:48:46 +0700

    first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+import HelloWorld from "./Component/Hello World";
+import Fam from "./Component/Fam";
 
+const ComponentH1=()=>
+{
+  return(
+      <h1>
+        Hello World
+      </h1>
+  )
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <HelloWorld/>
+        <ComponentH1/>
+          <Fam/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
